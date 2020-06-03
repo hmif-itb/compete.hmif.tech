dbf95195828077e92f9899149b875b2f7475312c
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2020-06-03 12:16:14 +0700

    Added Tailwind and MDI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import Icon from '@mdi/react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
@@ -14,8 +15,11 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <div>
+      <Icon path={require('@mdi/js').mdiAccount} />
+    </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        white: '#F0F0F0',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
